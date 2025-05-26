9c5c7ce01bcbfe6cfcfc339f27a26f179efe604c
sharkioso <tetkin04@mail.ru>
2025-05-27 00:40:10 +0500

    создана дто для страниц

--- backend/src/Core/Entities/Block.cs
+++ backend/src/Core/Entities/Block.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace PVDNOTE.Backend.Core.Entities;
 
 public class Block
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public int PageId { get; set; }
     public string Context { get; set; }
--- backend/src/Core/Entities/Page.cs
+++ backend/src/Core/Entities/Page.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace PVDNOTE.Backend.Core.Entities;
 
 public class Page
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Title { get; set; }
     public int WorkSpaceId { get; set; }
