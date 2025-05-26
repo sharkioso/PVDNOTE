cdab86ab1e0a92869c9c476ccb065430aad87a5e
sharkioso <tetkin04@mail.ru>
2025-05-27 00:37:12 +0500

    добавлена ещё одна миграция

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eebc9e484e070deaf798fe2e4c811d74fb8add03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b340acf6c910260842f310c11fbb1416886df6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
